9fbbd0f6e62399cf380f6ff1993c40c0863f737f
Peter Arato <it.arato@gmail.com>
2022-01-18 22:27:29 -0500

    Some doodling

--- lib/binner.rb
+++ lib/binner.rb
@@ -1,7 +1,47 @@
 # frozen_string_literal: true
 # typed: strict
 
-extend(T::Sig)
+class Binner
+  #
+  # Binner is the starting point that owns all type-codec information.
+  # It's not a root - but more a registry for types.
+  #
 
-module Binner
+  extend(T::Sig)
+
+  class TypeVersionedCodec
+    #
+    # Knows how to encode/decode a specific state (version) of that type.
+    #
+
+    extend(T::Sig)
+  end
+
+  class Type
+    #
+    # Contains information about one type.
+    #
+
+    extend(T::Sig)
+
+    sig { returns(Object) }
+    attr_reader(:klass)
+
+    sig do
+      params(
+        klass: Object,
+      ).void
+    end
+    def initialize(klass)
+      @klass = klass
+      @versioned_codecs = T.let([], T::Array[TypeVersionedCodec])
+    end
+  end
+
+  sig do
+    void
+  end
+  def initialize
+    @types = T.let([], T::Array[Type])
+  end
 end
---
+++ misc/scratch_01.rb
@@ -0,0 +1,38 @@
+#typed: false
+
+require_relative("../lib/binner")
+
+class User
+  attr_reader(:name)
+
+  def initialize(name)
+    @name
+  end
+end
+
+binner = Binner.new
+
+binner.register(type: User, version: 0) do
+  field(:name) do
+    # Return:
+    # - one object
+    # - can only contain primitive
+    encode do |obj|
+      obj.name
+    end
+
+    # # Input:
+    # # - same as return of encoder
+    # # Return:
+    # # - 
+    # decode do |value|
+    # end
+  end
+
+  # Return:
+  # - represented object
+  decode do |decoder|
+    raw_name = decoder.read(name)
+    User.new(raw_name)
+  end
+end
