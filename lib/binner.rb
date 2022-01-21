2f3e6254518461277d8be830b30dfe1fbb6950e0
Peter Arato <it.arato@gmail.com>
2022-01-20 23:05:11 -0500

    we might get somewhere

--- lib/binner.rb
+++ lib/binner.rb
@@ -11,7 +11,11 @@ class Binner
 
   extend(T::Sig)
 
-  class MissingCodecError < StandardError; end
+  class BinnerError < StandardError; end
+  class MissingCodecError < BinnerError; end
+  class NonSupportedVersionError < BinnerError; end
+  class VersionNotFoundError < BinnerError; end
+  class DecoderNotFoundError < BinnerError; end
 
   PrimitiveT = T.type_alias do
     T.any(
@@ -25,12 +29,90 @@ class Binner
     )
   end
 
-  class TypeVersionedCodec
-    #
-    # Knows how to encode/decode a specific state (version) of that type.
-    #
-
+  class FieldDecoder
     extend(T::Sig)
+    extend(T::Generic)
+
+    TargetT = type_member
+
+    sig { returns(Integer) }
+    attr_reader(:version)
+
+    sig { returns(T.proc.params(obj: PrimitiveT).returns(TargetT)) }
+    attr_reader(:decoder)
+
+    sig do
+      params(
+        version: Integer,
+        decoder: T.proc.params(obj: PrimitiveT).returns(TargetT),
+      ).void
+    end
+    def initialize(version, decoder)
+      @version = version
+      @decoder = decoder
+    end
+  end
+
+  class Field
+    extend(T::Sig)
+    extend(T::Generic)
+
+    TargetT = type_member
+
+    sig do
+      params(
+        name: Symbol,
+        from_version: Integer,
+        to_version: Integer,
+        encoder: T.proc.params(obj: TargetT).returns(PrimitiveT),
+      ).void
+    end
+    def initialize(name:, from_version:, to_version:, encoder:)
+      @name = name
+      @from_version = from_version
+      @to_version = to_version
+      @encoder = encoder
+      @decoders = T.let([], T::Array[FieldDecoder[TargetT]])
+    end
+
+    sig do
+      params(
+        obj: TargetT,
+        version: Integer, # Coming from the parent type codec.
+      ).returns(PrimitiveT)
+    end
+    def encode(obj, version)
+      {
+        version: version,
+        data: @encoder.call(obj),
+      }
+    end
+
+    sig do
+      params(
+        raw: { version: Integer, data: PrimitiveT },
+      ).returns(TargetT)
+    end
+    def decode(raw)
+      version = raw[:version]
+      raise(VersionNotFoundError) unless version
+      raise(NonSupportedVersionError) unless part_of_version?(version)
+
+      decoder = @decoders.find { |d| d.version == version }
+      raise(DecoderNotFoundError) unless decoder
+
+      data = raw[:data]
+      decoder.decoder.call(data)
+    end
+
+    sig do
+      params(
+        version: Integer,
+      ).returns(T::Boolean)
+    end
+    def part_of_version?(version)
+      @from_version <= version && version <= @to_version
+    end
   end
 
   class Type
@@ -53,7 +135,7 @@ class Binner
     end
     def initialize(klass)
       @klass = klass
-      @versioned_codecs = T.let([], T::Array[TypeVersionedCodec])
+      @fields = T.let([], T::Array[Field[T.untyped]])
     end
 
     sig do
@@ -62,7 +144,9 @@ class Binner
       ).returns(PrimitiveT)
     end
     def encode(obj)
-      raise(NotImplementedError)
+      # raise(NotImplementedError)
+
+
     end
 
     sig do
--- misc/scratch_01.rb
+++ misc/scratch_01.rb
@@ -2,31 +2,44 @@
 
 require_relative("../lib/binner")
 
+class Name
+  def initialize(name, age); end
+end
+
 class User
   attr_reader(:name)
 
-  def initialize(name)
-    @name
+  def initialize(name, weight)
+    @name = name
+    @weight = weight
   end
 end
 
 binner = Binner.new
 
-binner.register(type: User, version: 0) do
-  field(:name) do
-    # Return:
-    # - one object
-    # - can only contain primitive
-    encode do |obj|
-      obj.name
+binner.register(type: User, version: 2) do
+  field(:name, from_version: 0) do
+    # Always only 1 latest encoder is needed.
+    encode do |obj, encoder|
+      encoder.set(:name, obj.name)
+      encoder.set(:age, obj.age)
     end
 
-    # # Input:
-    # # - same as return of encoder
-    # # Return:
-    # # -
-    # decode do |value|
-    # end
+    add_decoder(0) do |decoder|
+      Name.new(decoder.get(:name), -1)
+    end
+
+    add_decoder(1) do |decoder|
+      Name.new(decoder.get(:name), decoder.get(:age))
+    end
+  end
+
+  field(:age, from_version: 0, until_version: 0) do
+    # ...
+  end
+
+  field(:weight, from_version: 2) do
+
   end
 
   # Return:
