94587e8886b93e192ea617bdd268440389fe3d8a
Peter Arato <it.arato@gmail.com>
2022-01-22 14:52:54 -0500

    Typed wrappers

---
+++ misc/scratch_02.rb
@@ -0,0 +1,33 @@
+#typed: false
+
+require_relative("../lib/binner")
+require "pp"
+
+class User
+  attr_reader(:name)
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+binner = Binner.new()
+
+user_type = Binner::Type[User].new(User, 0)
+
+user_name_field = Binner::Field[String].new(
+  name: :name,
+  from_version: 0,
+  to_version: nil,
+  encoder: ->(s) { s.name },
+)
+
+user_type.add_field(user_name_field)
+
+binner.add_type(user_type)
+
+encoded = binner.encode(User.new("Steve"))
+pp encoded
+
+decoded = binner.decode(encoded)
+pp decoded
