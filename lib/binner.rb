70e8ddfff7b5d9dfde7d0f0366368c55964655cf
Peter Arato <it.arato@gmail.com>
2022-01-22 14:52:43 -0500

    Typed wrappers

--- lib/binner.rb
+++ lib/binner.rb
@@ -17,16 +17,15 @@ class Binner
   class VersionNotFoundError < BinnerError; end
   class DecoderNotFoundError < BinnerError; end
 
-  PrimitiveT = T.type_alias do
-    T.any(
-      T::Boolean,
-      Numeric,
-      String,
-      NilClass,
-      # Sorbet does not allow type recursion yet.
-      T::Array[Object],
-      T::Hash[Object, Object],
-    )
+  class FieldWrapper < T::Struct
+    const(:version, Integer)
+    const(:data, T.untyped)
+  end
+
+  class TypeWrapper < T::Struct
+    const(:version, Integer)
+    const(:type, Class)
+    const(:data, T::Hash[Symbol, FieldWrapper])
   end
 
   class FieldDecoder
@@ -38,13 +37,13 @@ class Binner
     sig { returns(Integer) }
     attr_reader(:version)
 
-    sig { returns(T.proc.params(obj: PrimitiveT).returns(TargetT)) }
+    sig { returns(T.proc.params(obj: FieldWrapper).returns(TargetT)) }
     attr_reader(:decoder)
 
     sig do
       params(
         version: Integer,
-        decoder: T.proc.params(obj: PrimitiveT).returns(TargetT),
+        decoder: T.proc.params(obj: FieldWrapper).returns(TargetT),
       ).void
     end
     def initialize(version, decoder)
@@ -59,12 +58,15 @@ class Binner
 
     TargetT = type_member
 
+    sig { returns(Symbol) }
+    attr_reader(:name)
+
     sig do
       params(
         name: Symbol,
         from_version: Integer,
-        to_version: Integer,
-        encoder: T.proc.params(obj: TargetT).returns(PrimitiveT),
+        to_version: T.nilable(Integer),
+        encoder: T.proc.params(obj: TargetT).returns(FieldWrapper),
       ).void
     end
     def initialize(name:, from_version:, to_version:, encoder:)
@@ -79,18 +81,18 @@ class Binner
       params(
         obj: TargetT,
         version: Integer, # Coming from the parent type codec.
-      ).returns(PrimitiveT)
+      ).returns(FieldWrapper)
     end
     def encode(obj, version)
-      {
+      FieldWrapper.new(
         version: version,
         data: @encoder.call(obj),
-      }
+      )
     end
 
     sig do
       params(
-        raw: { version: Integer, data: PrimitiveT },
+        raw: { version: Integer, data: FieldWrapper },
       ).returns(TargetT)
     end
     def decode(raw)
@@ -111,7 +113,7 @@ class Binner
       ).returns(T::Boolean)
     end
     def part_of_version?(version)
-      @from_version <= version && version <= @to_version
+      @from_version <= version && (@to_version.nil? || version <= @to_version)
     end
   end
 
@@ -130,28 +132,50 @@ class Binner
 
     sig do
       params(
-        klass: Object,
+        klass: Class,
+        # Represents the version currently at.
+        version: Integer,
       ).void
     end
-    def initialize(klass)
+    def initialize(klass, version)
       @klass = klass
+      @version = version
       @fields = T.let([], T::Array[Field[T.untyped]])
     end
 
     sig do
       params(
-        obj: TargetT,
-      ).returns(PrimitiveT)
+        field: Field[TargetT],
+      ).void
     end
-    def encode(obj)
-      # raise(NotImplementedError)
-
-
+    def add_field(field)
+      @fields << field
     end
 
     sig do
       params(
-        raw: PrimitiveT,
+        obj: TargetT,
+      ).returns(TypeWrapper)
+    end
+    def encode(obj)
+      out = TypeWrapper.new(
+        version: @version,
+        type: @klass,
+        data: {},
+      )
+
+      @fields.each do |field|
+        if field.part_of_version?(@version)
+          out.data[field.name] = field.encode(obj, @version)
+        end
+      end
+
+      out
+    end
+
+    sig do
+      params(
+        raw: TypeWrapper,
       ).returns(TargetT)
     end
     def decode(raw)
@@ -168,8 +192,17 @@ class Binner
 
   sig do
     params(
+      type: Type[T.untyped],
+    ).void
+  end
+  def add_type(type)
+    @types << type
+  end
+
+  sig do
+    params(
       obj: T.untyped,
-    ).returns(PrimitiveT)
+    ).returns(TypeWrapper)
   end
   def encode(obj)
     t = @types.find do |t|
@@ -179,5 +212,26 @@ class Binner
     raise(MissingCodecError) unless t
 
     t.encode(obj)
+  end
+
+  sig do
+    params(
+      raw: TypeWrapper,
+    ).returns(T.untyped)
+  end
+  def decode(raw)
+    klass = raw.type
+    type_for(klass).decode(raw)
+  end
+
+  private
+
+  sig do
+    params(
+      klass: Class,
+    ).returns(Type[T.untyped])
+  end
+  def type_for(klass)
+    @types.find { |t| klass == t.klass } || raise(MissingCodecError)
   end
 end
